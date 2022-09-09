e31917c41861580a468fcfbf2d3ab336cfdfe803
madokscharles <charlesmadoks@gmail.com>
2022-09-08 17:07:49 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - main block - quote and that piece of art is useful
+ * Return: 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
