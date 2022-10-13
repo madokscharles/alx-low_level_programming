6b9015309e271a3d568671d9df6d33cb47b22a7b
madokscharles <charlesmadoks@gmail.com>
2022-10-13 16:27:48 -0700

    correction

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,11 +7,4 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
-typedef struct fm
-{
-	char *fm;
-	void (*p)();
-} fm_t;
-
-
 #endif
