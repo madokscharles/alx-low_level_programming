7f0dc210c88b7803878ef5dc87aea8d1b1752c2c
madokscharles <charlesmadoks@gmail.com>
2022-10-13 15:58:24 -0700

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,98 @@
 #include "variadic_functions.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include <stdarg.h>
+
+/**
+ * print_c - prints characters
+ * @list: arguments
+ * @sep: Separator
+ */
+
+void print_c(va_list list, char *sep)
+{
+	printf("%s%c", sep, va_arg(list, int));
+}
+
+/**
+ * print_i - prints integers
+ * @list: arguments
+ * @sep: Separator
+ */
+
+void print_i(va_list list, char *sep)
+{
+	printf("%s%d", sep, va_arg(list, int));
+}
+
+/**
+ * print_f - prints floats
+ * @list: arguments
+ * @sep: Separator
+ */
+
+void print_f(va_list list, char *sep)
+{
+	printf("%s%f", sep, va_arg(list, double));
+}
+
+/**
+ * print_s - prints strings
+ * @list: arguments
+ * @sep: Separator
+ */
+
+void print_s(va_list list, char *sep)
+{
+	char *s;
+
+	s = va_arg(list, char *);
+
+	if (s == NULL)
+		s = "(nil)";
+
+	printf("%s%s", sep, s);
+}
 
 /**
  * print_all - prints anything
- * @format: list of types of arguments
- * Return: no return
+ * @format: list of types of arguments passed to the function
  */
 
 void print_all(const char * const format, ...)
 {
-	va_list arg_point;
-	unsigned int i = 0, expr = 0;
-	char *ap;
+	va_list ap;
+	char *sep;
+	int i, j;
 
-	va_start(arg_point, format);
+	fm_t fm[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_f},
+		{"s", print_s},
+		{NULL, NULL}
+	};
 
-	while (format && format[i])
+	va_start(ap, format);
+
+	i = 0;
+	sep = "";
+
+	while (format != NULL && format[i] != '\0')
 	{
-		if (expr)
-			printf(", ");
-		switch (format[i])
+		j = 0;
+
+		while (j < 4)
 		{
-			case 'c':
-				printf("%c", va_arg(arg_point, int));
-				break;
-			case 'i':
-				printf("%i", va_arg(arg_point, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(arg_point, double));
-				break;
-			case 's':
-				arg = va_arg(arg_point, char *);
-				if (ap)
-				{
-					printf("%s", ap);
-					break;
-				}
-				printf("%p", ap);
-				break;
-			default:
-				expr = 0;
-				i++;
-				continue;
+			if (format[i] == *(fm[j]).fm)
+			{
+				fm[j].p(ap, sep);
+				sep = ", ";
+			}
+			j++;
 		}
-		expr = 1;
 		i++;
 	}
 	printf("\n");
-	va_end(arg_point);
+
+	va_end(ap);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,12 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+typedef struct fm
+{
+	char *fm;
+	void (*p)();
+} fm_t;
+
+
 #endif
