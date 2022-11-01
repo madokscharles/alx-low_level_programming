ae70b94cbd5af3027540637706358fac428ca8f4
madokscharles <charlesmadokes@gmail.com>
2022-10-31 17:03:21 -0700

    A program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,52 +1,84 @@
 #include "main.h"
 
 /**
- * main - copies the content of a file to another file
- * @argc: number of arguments
- * @argv: the argument pointer
- *
- * Return: zero on success
+ * _error - Read file
+ * @e: Error number
+ * @filename: File name
  */
 
-int main(int argc, char **argv)
+void _error(int e, char *filename)
 {
-	int copyfile, newfile;
+	if (e == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	if (e == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+}
 
-	if (argc != 3)
+/**
+ * cp - Copies the content of a file to another file
+ * @file_from: Name of the source file
+ * @file_to: Name of the destination file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+void cp(char *file_from, char *file_to)
+{
+	int fd_read, res_read, fd_write, res_write;
+	char *buf[1024];
+
+	fd_read = open(file_from, O_RDONLY);
+	if (fd_read < 0)
+		_error(98, file_from);
+	fd_write = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_write < 0)
+	{
+		close(fd_read);
+		_error(99, file_to);
+	}
+	do {
+		res_read = read(fd_read, buf, 1024);
+		if (res_read < 0)
+			_error(98, file_from);
+		res_write = write(fd_write, buf, res_read);
+		if (res_write < res_read)
+			_error(99, file_to);
+	} while (res_write == 1024);
+	if (close(fd_read) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_read);
+		close(fd_write);
+		exit(100);
+	}
+	if (close(fd_write) < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the content of a file to another file
+ * @ac: Argument count
+ * @av: argument values
+ *
+ * Return: 0 on succes, -1 on error
+ */
+
+int main(int ac, char *av[])
+{
+	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	copyfile = open(argv[1], O_RDONLY);
-	if (copyfile == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	newfile = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
-	if (newfile == -1)
-		newfile = open(argv[2], O_TRUNC | O_WRONLY);
-	if (newfile == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(copyfile);
-		exit(99);
-	}
-
-	copy_text_file(copyfile, newfile, argv[1], argv[2]);
-
-	if (close(copyfile) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", copyfile);
-		close(newfile);
-		exit(100);
-	}
-	if (close(newfile) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", newfile);
-		exit(100);
-	}
+	cp(av[1], av[2]);
 
 	return (0);
 }
